59ee98537755ce1d063b29e7d298d0a4723b845f
Matt Helker <mdhelker23@gmail.com>
2025-09-17 03:21:35 -1000

    chick 108

--- src/app/performers/[id]/page.tsx
+++ src/app/performers/[id]/page.tsx
@@ -1,15 +1,12 @@
-
 import { PerformerDetailClient } from '@/components/performer-detail-client';
 
 // This page component now only passes the ID to the client component.
 // The actual data fetching will happen on the client-side.
 export default function PerformerDetailPage({ params }: { params: { id: string } }) {
-  const { id } = params;
-
   return (
     <div className="container mx-auto py-8">
       {/* Pass the performer ID to the client component */}
-      <PerformerDetailClient performerId={id} />
+      <PerformerDetailClient performerId={params.id} />
     </div>
   );
 }
